93522d09fe1c13dea3f756ebeb2d05be1f7357c1
Clayton Pereira <x851766@gruposantander.com>
2022-11-22 10:44:52 -0300

    feat: adding comment on getBean Method

--- springDemoOne/src/com/claytonpereira/springDemo/SpringHelloApp.java
+++ springDemoOne/src/com/claytonpereira/springDemo/SpringHelloApp.java
@@ -8,7 +8,9 @@ public class SpringHelloApp {
 		// load the spring xml configuration file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		// retrieve beans from spring container
-
+        
+		// When we pass the interface "Coach.class" to the method, behind the scenes 
+		// Spring will cast the object for you
 		Coach theCoach = context.getBean("myCoach", Coach.class);
 		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
