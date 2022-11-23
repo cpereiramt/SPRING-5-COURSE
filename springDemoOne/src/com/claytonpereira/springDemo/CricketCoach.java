9e0beae0025fe62ed12ffd782461e1beabff84aa
Clayton Pereira <x851766@gruposantander.com>
2022-11-23 08:53:30 -0300

    adding literal values

--- springDemoOne/src/com/claytonpereira/springDemo/CricketCoach.java
+++ springDemoOne/src/com/claytonpereira/springDemo/CricketCoach.java
@@ -3,7 +3,27 @@ package com.claytonpereira.springDemo;
 public class CricketCoach implements Coach {
     // dependency to be injected in setter
 	private FortuneService fortuneService;
-	 
+	private String emailAddress;
+	private String team;
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("Email address is " + emailAddress);
+		this.emailAddress = emailAddress;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("team name is " + team);
+		this.team = team;
+	}
+
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside no-arg constructor");
 	}
